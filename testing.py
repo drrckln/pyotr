e42ae37ed054b6806aaf19f8c1bc820a05afb648
Derrick Lin <drrckln@gmail.com>
2012-07-24 16:32:21 -0400

    can announce to tracker!

--- testing.py
+++ testing.py
@@ -1,16 +1,48 @@
 import bencode
+import requests
+import sha
 
-torrent = bencode.bdecode(open('/Users/derrickl/Desktop/Sapolsky.mp4.torrent', 'rb').read())
+def decode(file):
+    torrent = bencode.bdecode(open('Sapolsky.mp4.torrent', 'rb').read())
+    return torrent
 
-print torrent.keys()
+def announce(file):
+    info_hash = getdicthash(file)
+    torrent = decode(file)
+    payload = {'info_hash': info_hash,
+               'peer_id':'-TR2610-dfhmjb0skee6',
+               'port':'6881',
+               'uploaded':'0',
+               'downloaded':'0',
+               'key':'2c4dec5f',
+               'left':'0',
+               'no_peer_id':'0',
+               'event':'started',
+               'numwant':'80'}
 
-print torrent['info'].keys()
+    response = requests.get(torrent['announce'], params = payload)
+    print response.content
+    print response.url
 
-pieces = torrent['info']['pieces']
 
-print len(pieces)
+def getdicthash(file):
+    contents = open(file, 'rb').read()
+    start = contents.index('4:info') + 6
+    end = -1
+    dictliteral = contents[start:end]
+    dictsha = sha.new(dictliteral)
+    return dictsha.digest()
 
-pieceshas = []
 
-for i in range(len(pieces)/20):
-    
+
+
+announce('Sapolsky.mp4.torrent')
+
+
+def spliceshas(torrent):
+    pieceshas = []
+
+    for i in range(len(pieces)/20):
+       pieceshas.append(pieces[20*i:20*(i+1)])
+
+    print pieceshas[0:3]
