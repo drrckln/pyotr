ac2a61df14e0be6392203e53b35dc3dc7b3620b7
Derrick Lin <drrckln@gmail.com>
2012-08-06 21:50:20 -0400

    can download a block now and SHA1 check

--- testing.py
+++ testing.py
@@ -3,18 +3,43 @@ import requests
 import sha
 import struct
 import socket
-import twisted
 
 def decode(file):
+    ''' Decodes the bencoded file, returns decoded dictionary '''
     torrent = bencode.bdecode(open(file, 'rb').read())
     return torrent
 
-def handshake(file):
-    info_hash = getdicthash(file)
-    # open a socket, send this:
-    return chr(19) + 'BitTorrent Protocol' + '\0'*8 + info_hash + '-TR2610-dfhmjb0skee6'
+def recvall(socket, expected):
+    ''' Allows you to receive an expected amount off a socket '''
+    data = ''
+    while True:
+        newdata = socket.recv(expected)
+        data += newdata
+        expected = expected - len(newdata)
+        if expected == 0:
+            break
+    return data
+
+def spliceshas(torrent):
+    ''' Splices the SHA1 keys into a list '''
+    tor = bencode.bdecode(open(torrent, 'rb').read()) 
+    print tor['info'].keys()
+    print tor['info']['piece length']
+    pieces = tor['info']['pieces']
+    pieceshas = []
+
+    for i in range(len(pieces)/20):
+       pieceshas.append(pieces[20*i:20*(i+1)])
+
+    return pieceshas
+
+
+''' Tracker '''
 
 def announce(file):
+    ''' Announces to a tracker 
+    
+    Currently returns 1 peer's IP and port, hardcoded '''
     info_hash = getdicthash(file)
     torrent = decode(file)
     payload = {'info_hash': info_hash,
@@ -30,15 +55,12 @@ def announce(file):
                'numwant':'30'}
 
     response = requests.get(torrent['announce'], params = payload)
-    print response.content
     reply = bencode.bdecode(response.content)
-    print reply.keys()
     print 'peers: ' + repr(reply['peers'])
-    # print 'min interval: ' + str(reply['min interval'])
     print 'complete: ' + str(reply['complete'])
     print 'interval: ' + str(reply['interval'])
-    # print 'downloaded: ' + str(reply['downloaded'])
     print 'incomplete: ' + str(reply['incomplete'])
+    
     data = reply['peers']
     multiple = len(data)/6
     print struct.unpack("!" + "LH"*multiple, data)
@@ -46,17 +68,95 @@ def announce(file):
         print socket.inet_ntop(socket.AF_INET, data[6*i:6*i+4]) + ":" + repr(struct.unpack("!H", data[6*i+4:6*i+6])[0])
     ip =  socket.inet_ntop(socket.AF_INET, data[0:4])
     port = int(repr(struct.unpack("!H", data[4:6])[0]))
+    return (ip, port)
 
-    msg = handshake('Sapolsky.mp4.torrent')
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
-    s.connect((ip, 51413))
+
+''' Peer '''
+
+def handshake(socket):
+    ''' Initiates handshake with peer '''
+    info_hash = getdicthash('Sapolsky.mp4.torrent')    
+    msg = chr(19) + 'BitTorrent protocol' + '\x00'*8 + info_hash + '-TR2610-dfhmjb0skee6'
     s.send(msg)
-    print repr(msg)
-    print "Received: %s" % s.recv(1024)
-    
+    print "Handshake sent: ", repr(msg)
+    print "Handshake rcvd: %s" % repr(s.recv(4096))
+
+
+def make_request(piece, offset, length):
+    ''' Constructs msg for requesting a block from a peer
+
+    Need to add in begin, offset, length as vars '''
+    return struct.pack('!L', 13) + chr(6) + struct.pack('!LLL', piece, offset, 16384)
+
+
+
+def flagmsg(socket):
+    ''' Takes a bit off socket buffer; returns a tuple of the action and the data from a socket
+
+    BLOCKS'''
+    first  = socket.recv(4)
+    length = struct.unpack('!L', first)[0]
+    id_data = recvall(socket, length)
+    if id_data == '':
+        return
+    id = id_data[0]
+    data = id_data[1:]
+    if id == '\x00':
+        return ('choke', None)
+    elif id == '\x01':
+        return ('unchoke', None)
+    elif id == '\x02':
+        return ('interested', None)
+    elif id == '\x03':
+        return ('not interested', None)
+    elif id == '\x04':
+        return ('have', data)
+    elif id == '\x05':
+        return ('bitfield', data)
+    elif id == '\x06':
+        return ('request', data)
+    elif id == '\x07':
+        return ('piece', data)
+    elif id == '\x08':
+        return ('cancel', data)
+
+
+def receive_loop():
+    piece_data = [None]*131072
+    while True:
+        flag, data = flagmsg(s)
+        print flag
+        if flag == 'bitfield':
+            num = int(data.encode('hex'), 16)
+            bitfield = bin(num)[2:len(spliceshas('Sapolsky.mp4.torrent'))+2]
+            bfield = [ (True if x == '1' else False) for x in bitfield ]
+            print bitfield
+            print bfield[0:10]
+        elif flag == 'unchoke':
+            ''' If unchoked, send a request! '''
+            print 'unchoked!'
+            s.sendall(make_request(0, 0, 16384))
+            last_req_length = 16384
+            # we don't actually need this, can get from length of data. attribute it?
+        elif flag == 'piece':
+            piece, offset = struct.unpack('!LL', data[:8])
+            print repr(data[:20])
+            print piece, offset
+            print len(data[8:])
+            piece_data[offset:offset+last_req_length] = data[8:]
+            if None not in piece_data:
+                print "yay!"
+                break
+            first_blank = piece_data.index(None)
+            size_left = piece_data.count(None)
+            s.sendall(make_request(0, first_blank, min(16384, size_left)))
+            last_req_length = min(16384, size_left)
+    return piece_data
+
 
 def getdicthash(file):
+    ''' Returns the SHA1 hash of the 'info' key in the metainfo file '''
     contents = open(file, 'rb').read()
     start = contents.index('4:info') + 6
     end = -1
@@ -67,19 +167,20 @@ def getdicthash(file):
 
 
 
-announce('Sapolsky.mp4.torrent')
 
 
-def spliceshas(torrent):
-    tor = bencode.bdecode(open(torrent, 'rb').read()) 
-    print tor['info'].keys()
-    pieces = tor['info']['pieces']
-    pieceshas = []
+''' MAIN '''
 
-    for i in range(len(pieces)/20):
-       pieceshas.append(pieces[20*i:20*(i+1)])
-
-    print pieceshas[0:3]
+pieceshas = spliceshas('Sapolsky.mp4.torrent')
+ip, port = announce('Sapolsky.mp4.torrent')
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+s.connect((ip, 51413))
+handshake(s)
+first_block = receive_loop()
+print pieceshas[0]
+first_block = "".join(first_block)
+first_block = sha.new(first_block).digest()
+print first_block
 
 
 
@@ -101,53 +202,4 @@ if __name__ == '__main__':
     for peer in peerList
         fac.buildProtocol(peer)
 
-'''
-def handshake(file):
-    info_hash = getdicthash(file)
-    # open a socket, send this:
-    return chr(19) + 'BitTorrent Protocol' + '\x00'*8 + info_hash + '-TR2610-dfhmjb0skee6'
-
-'''
-
-
-def message(peer, length, id, payload):
-    # open socket, send
-    # lengthprefix messageID payload
-    # length 4 byte big endian
-    # messageID 1 decimal byte
-
-def keepalive(peer):
-    message(peer, 0, 0, 0)
-
-def choke(peer):
-    message(peer, 1, 0, 0)
-
-def unchoke(peer):
-    message(peer, 1, 1, 0)
-
-def interested(peer):
-    message(peer, 1, 2, 0)
-
-def notinterested(peer):
-    message(peer, 1, 3, 0)
-
-def have(peer):
-    message(peer, 5, 4, pieceindex)
-
-def bitfield(peer):
-    message(peer, 0001+X, 5, bitfield)
-
-def request(peer):
-    message(peer, 0013, 6, index/begin/length)
-    # index = integer specifying zero-based piece index
-    # begin = integer specifying the zero-based byte offset within the piece
-    # length = integer specifying the requested length
-
-def piece(peer):
-    message(peer, 0009+X, 7, index/begin/block)
-    # block = block of data, which is a subset of the piece specified by index
-
-def cancel(peer):
-    message(peer, 0013, 8, index/begin/length)
-    # used to cancel block requests. payload identical to 'request' message
 '''
